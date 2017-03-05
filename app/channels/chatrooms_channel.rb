068793e5ff555b8062faed6f63d6b2d438d0d00b
kbanico <code4uguam@gmail.com>
2017-03-05 12:10:08 +1000

    initial commit

---
+++ app/channels/chatrooms_channel.rb
@@ -0,0 +1,20 @@
+class ChatroomsChannel < ApplicationCable::Channel
+  def subscribed
+    # stream_from "some_channel"
+    current_user.chatrooms.each do |chatroom|
+      stream_from "chatrooms:#{chatroom.id}"
+    end
+  end
+
+  def unsubscribed
+    # Any cleanup needed when channel is unsubscribed
+    stop_all_streams
+  end
+
+  def send_message(data)
+    @chatroom = Chatroom.find(data["chatroom_id"])
+    message   = @chatroom.messages.create(body: data["body"], user: current_user)
+    MessageRelayJob.perform_later(message)
+    Rails.logger.info data
+  end
+end
